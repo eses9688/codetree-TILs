21c5eafc23c73acdad400e30d9b184279371bbec
ESP <128010614+eses9688@users.noreply.github.com>
2024-07-31 22:47:33 +0900

    Title: 점수 입출력, Time: 158ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240731/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int score = s.nextInt();
+        System.out.println("Your score is "+score+" point.");
+    }
+}
