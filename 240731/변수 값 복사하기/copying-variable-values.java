81bb6f867b586456f8ad2f1529b9ddfc0e93671a
ESP <128010614+eses9688@users.noreply.github.com>
2024-07-31 22:40:02 +0900

    Title: 변수 값 복사하기, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240731/변수 값 복사하기/copying-variable-values.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a, b, c;
+        a = 1;
+        b = 2;
+        c = 3;
+        a = b = c;
+        System.out.print(a+" ");
+        System.out.print(b+" ");
+        System.out.print(c);
+    }
+}
