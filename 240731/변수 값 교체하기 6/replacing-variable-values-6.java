4c6337e8a82d02eb0d10c17c621e42b881f9cd09
ESP <128010614+eses9688@users.noreply.github.com>
2024-07-31 22:36:47 +0900

    Title: 변수 값 교체하기 6, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240731/변수 값 교체하기 6/replacing-variable-values-6.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int a, b, tmp;
+        a = 2;
+        b = 5;
+        tmp = a;
+        a = b;
+        b = tmp;
+        System.out.println(a);
+        System.out.print(b);
+    }
+}
