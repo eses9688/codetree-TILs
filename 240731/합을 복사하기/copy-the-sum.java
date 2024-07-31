e9d52544813ac0f94eeaa65f254c39af6a8b2412
ESP <128010614+eses9688@users.noreply.github.com>
2024-07-31 22:42:08 +0900

    Title: 합을 복사하기, Time: 87ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240731/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int a, b, c, sum;
+        a = 1;
+        b = 2;
+        c = 3;
+        sum = a + b + c;
+        a = b = c = sum;
+        System.out.print(a+" "+b+" "+c);
+    }
+}
