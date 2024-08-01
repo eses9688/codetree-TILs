b7c159037f26b9c0da31be289c72a7dfe323532b
ESP <128010614+eses9688@users.noreply.github.com>
2024-08-01 16:32:37 +0900

    Title: 1시간 뒤 시간 출력, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240801/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        s.useDelimiter(":");
+        int a = s.nextInt();
+        int b = s.nextInt();
+        System.out.println((a+1)+":"+b);
+    }
+}
