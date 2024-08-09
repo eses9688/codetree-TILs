f221461ea4f16499f099c52da9f4b01034539b25
ESP <128010614+eses9688@users.noreply.github.com>
2024-08-09 15:26:15 +0900

    Title: 범위 밖의 수, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240809/범위 밖의 수/number-out-of-range.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+
+        if (a < 10 || a >20) {
+            System.out.print("yes");
+        }
+        else {
+            System.out.print("no");
+        }
+    }
+}
