07a649031c874d793d81a15491cbc98b8973a948
ESP <128010614+eses9688@users.noreply.github.com>
2024-08-22 19:20:56 +0900

    Title: a 부터 b 까지, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240822/a 부터 b 까지/a-to-b.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int i = a;
+
+        System.out.print(i+" ");
+        while (i < b) {
+            if (i%2 == 1) {
+                i *= 2;
+            }
+            else {
+                i += 3;
+            }
+            System.out.print(i+" ");
+        }
+
+    }
+}
