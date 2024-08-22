4605a3722af31f6d46ee3fffb92ab23ee70c2045
ESP <128010614+eses9688@users.noreply.github.com>
2024-08-22 18:23:07 +0900

    Title: 숫자의 증감, Time: 3029ms, Memory: 37MB, Status: Time Limit Exceed - CodeTree

---
+++ 240822/숫자의 증감/increasing-and-decreasing-numbers.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        char c = s.next().charAt(0);
+        int n = s.nextInt();
+
+        if (c == 'A') {
+            for (int i = 1; i <= n; i++) {
+                System.out.print(i+" ");
+            }
+        }
+        else {
+            for (int i = n; i >= 1; i++) {
+                System.out.print(i+" ");
+            }
+        }
+
+    }
+}
