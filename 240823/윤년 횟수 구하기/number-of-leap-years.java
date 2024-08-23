38ccdb07a3ed0243dd575b49d1f35079ed68f451
ESP <128010614+eses9688@users.noreply.github.com>
2024-08-23 21:27:12 +0900

    Title: 윤년 횟수 구하기, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240823/윤년 횟수 구하기/number-of-leap-years.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int cnt = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i%4 == 0) {
+                if (i%100 == 0 && i%400 != 0) {
+                    cnt += 0;
+                }
+                else {
+                    cnt++;
+                }
+            }
+
+        }
+        System.out.print(cnt);
+
+    }
+}
