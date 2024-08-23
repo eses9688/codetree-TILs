c3f9daa0b787d701215d98b7c7ef3ed3224b4164
ESP <128010614+eses9688@users.noreply.github.com>
2024-08-23 21:30:53 +0900

    Title: a부터 b까지의 합, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240823/a부터 b까지의 합/sum-from-a-to-b.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        int b = s.nextInt();
+        int sum = 0;
+        for (int i = a; i <= b; i++) {
+            sum += i;
+        }
+        System.out.print(sum);
+
+    }
+}
