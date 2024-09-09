aafa12a7265c5b785532a029399fbd714eb89ea5
ESP <128010614+eses9688@users.noreply.github.com>
2024-09-09 23:29:50 +0900

    Title: 구구단 만들기 2, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240909/구구단 만들기 2/print-multiplication-table-2.java
+++ 240909/구구단 만들기 2/print-multiplication-table-2.java
@@ -5,14 +5,18 @@ public class Main {
         Scanner s = new Scanner(System.in);
         int a = s.nextInt();
         int b = s.nextInt();
+        int cnt;
 
         for (int i = 2; i < 10; i+=2) {
-            for (int j = b; j >= b - a; j--) {
+            cnt = 0;
+            for (int j = b; j >= a; j--) {
                 System.out.print(j + " * " + i + " = " + (i * j));
-                if (j != a) {
+                cnt++;
+                if (cnt != b - a + 1) {
                     System.out.print(" / ");
                 }
             }
+            
             System.out.println();
         }
     }
