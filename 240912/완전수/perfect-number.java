3b6eed28afa20c6b927e31c60f90eaf05b94ab67
ESP <128010614+eses9688@users.noreply.github.com>
2024-09-12 21:55:15 +0900

    Title: 완전수, Time: 177ms, Memory: 10MB, Status: Passed - CodeTree

--- 240912/완전수/perfect-number.java
+++ 240912/완전수/perfect-number.java
@@ -5,16 +5,24 @@ public class Main {
         Scanner s = new Scanner(System.in);
         int start = s.nextInt();
         int end = s.nextInt();
-        int sum = 1;
         int cnt = 0;
 
-        for (int i = 2; i * i < end && sum <= end; i++) {
-            if (end % i == 0) { 
-                sum += i + end / i; 
-            }
-            cnt++;
-        }
+        for (int i = start; i <= end; i++) {
+            int sum = 1;
 
+            for (int j = 2; j * j <= i; j++) {
+                if (i % j == 0) {
+                    if (j * j == i) {
+                        sum += j; 
+                    } else {
+                        sum += j + i / j;
+                    }
+                }
+            }
+            if (sum == i && i != 1) { 
+                cnt++;
+            }
+        }
         System.out.println(cnt);
     }
 }
