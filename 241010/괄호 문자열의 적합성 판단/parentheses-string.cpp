7daedfed6d45b9a31f3b70d9d92d1abe4bd3f87c
ESP <128010614+eses9688@users.noreply.github.com>
2024-10-10 20:15:30 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241010/괄호 문자열의 적합성 판단/parentheses-string.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<string> s;    // 빈 스택 추가
+    string str;         // str 배열 입력받음
+    cin >> str;
+    for (int i = 0; i< str.size(); i++) {
+        if (str[i] == '(') {    // str[i]에 '('가 있는 경우
+            s.push("(");        // 스택에 ( 추가
+        }
+        else {                  // '('가 없는 경우 = ')'인 경우
+            if (s.empty() == true) {    // 스택이 비어 있는 경우
+                cout << "No";
+                break;           // No 출력
+            }
+            s.pop();            // 
+        }
+    }
+    if (s.empty() == false) {
+        cout << "No";
+    }
+    else{
+        cout << "Yes";
+    }
+
+    return 0;
+}
