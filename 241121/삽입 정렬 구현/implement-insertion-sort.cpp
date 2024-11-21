262f19420de9141dd74e1deb33b28661ebf04ceb
ESP <128010614+eses9688@users.noreply.github.com>
2024-11-21 19:53:40 +0900

    Title: 삽입 정렬 구현, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241121/삽입 정렬 구현/implement-insertion-sort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, tmp;
+    cin >> n;
+    int arr[n];
+    // 입력
+    for (int i = 0; i < n; i++){
+      cin >> arr[i];
+    }
+    // 삽입 정렬
+    for (int i = 0; i < n - 1; i++){
+      int j = i - 1;
+      int key = arr[i];
+      while (j >= 0 && arr[j] > key){
+        arr[j + 1] = arr[j];
+        j--;
+      }
+      arr[j + 1] = key;
+    }
+    // 출력 
+    for (int i = 0; i < n; i++){
+      cout << arr[i] << " ";
+    }
+    return 0;
+}
