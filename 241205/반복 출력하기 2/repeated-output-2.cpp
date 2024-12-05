81eef21db733e704553d766e1c07f6c7cc6e7aeb
ESP <128010614+eses9688@users.noreply.github.com>
2024-12-05 15:15:21 +0900

    Title: 반복 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241205/반복 출력하기 2/repeated-output-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void Print(int n){
+    if(n == 0){
+        return;
+    }  
+    Print(n - 1);
+    cout << "HelloWorld" << endl;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    Print(n);
+    return 0;
+}
